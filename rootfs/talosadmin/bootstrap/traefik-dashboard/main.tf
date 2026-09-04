8425ab404f0e3fa14011ef027f134458f7c549da
ElevenNotes <github@11notes.ch>
2026-09-04 11:14:04 +0200

    feat: add dashboard redirect

--- rootfs/talosadmin/bootstrap/traefik-dashboard/main.tf
+++ rootfs/talosadmin/bootstrap/traefik-dashboard/main.tf
@@ -64,6 +64,24 @@ resource "kubernetes_manifest" "basic_auth_middleware" {
   }
 }
 
+resource "kubernetes_manifest" "traefik_dashboard_redirect_middleware" {
+  manifest = {
+    apiVersion = "traefik.io/v1alpha1"
+    kind = "Middleware"
+    metadata = {
+      name = "traefik-dashboard-redirect"
+      namespace = "traefik"
+    }
+    spec = {
+      redirectRegex = {
+        regex = "^(https?://[^/]+)/?$"
+        replacement = "$${1}/dashboard/"
+        permanent = true
+      }
+    }
+  }
+}
+
 resource "kubernetes_manifest" "traefik_dashboard_ingress" {
   manifest = {
     apiVersion = "traefik.io/v1alpha1"
@@ -71,12 +89,8 @@ resource "kubernetes_manifest" "traefik_dashboard_ingress" {
     metadata = {
       name = "traefik-dashboard"
       namespace = "traefik"
-      annotations = {
-        "cert-manager.io/cluster-issuer" = "letsencrypt-prod"
-      }
     }
     spec = {
-      entryPoints = ["websecure"]
       routes = [
         {
           match = "Host(`${trimspace(var.traefik_fqdn)}`) && (PathPrefix(`/dashboard`) || PathPrefix(`/api`))"
@@ -90,6 +104,22 @@ resource "kubernetes_manifest" "traefik_dashboard_ingress" {
           middlewares = [
             {
               name = kubernetes_manifest.basic_auth_middleware.manifest.metadata.name
+              namespace = "traefik"
+            }
+          ]
+        },
+        {
+          match = "Host(`${trimspace(var.traefik_fqdn)}`) && Path(`/`)"
+          kind = "Rule"
+          services = [
+            {
+              name = "api@internal"
+              kind = "TraefikService"
+            }
+          ]
+          middlewares = [
+            {
+              name = kubernetes_manifest.traefik_dashboard_redirect_middleware.manifest.metadata.name
               namespace = "traefik"
             }
           ]
